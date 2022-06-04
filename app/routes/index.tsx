5138853f26ad029bc5fa801458f492f6de96fdf4
Wilman Barrios <wilmanbarrios@gmail.com>
2022-06-03 20:09:14 -0400

    Improve profile image

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -20,14 +20,12 @@ export default function Index() {
   return (
     <main className="flex flex-col h-screen px-6 pt-12 md:p-0 md:max-w-xl md:mx-auto md:pt-10">
       <div className="flex flex-col items-center justify-center mb-6">
-        <div className="p-1 rounded-full bg-gradient-to-b from-white via-white to-black/10 shadow-md ring-1 ring-black/5">
-          <img
-            src="/profile_image.jpg"
-            loading="lazy"
-            alt="Wilman Barrios avatar"
-            className="rounded-full object-cover w-36 h-36"
-          />
-        </div>
+        <img
+          src="/profile_image.jpg"
+          loading="lazy"
+          alt="Wilman Barrios avatar"
+          className="rounded-full object-cover w-36 h-36 ring-1 ring-offset-4 ring-black/5 drop-shadow-md"
+        />
         <h1 className="text-4xl font-black text-slate-800 mt-4">
           Wilman Barrios
         </h1>
